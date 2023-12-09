dcc1ac85ed0aeddc469bd022ddcd3e1cecfef53c
Clément Vandendaelen <vandendaelenclement@gmail.com>
2023-12-09 11:40:47 +0100

    Day 09 completed

---
+++ src/main/kotlin/adventOfCode/y2023/day_09.kt
@@ -0,0 +1,71 @@
+package adventOfCode.y2023
+
+import adventOfCode.getLines
+import kotlin.system.measureTimeMillis
+
+data class Scan(
+    val report: List<Long>
+) {
+
+    fun extrapolateNumber(reversed: Boolean = false): Long {
+        val computations: MutableList<MutableList<Long>> = mutableListOf(
+            if (reversed) {
+                report.reversed()
+            } else {
+                report
+            }.toMutableList()
+        )
+
+        do {
+            val currentRow = computations.last()
+            computations.add(
+                currentRow.zipWithNext().map { (v1, v2) ->
+                    v2 - v1
+                }.toMutableList()
+            )
+        } while (!computations.last().all { it == 0L })
+
+        val reversedComputations = computations.reversed()
+
+        reversedComputations.forEachIndexed { index, longs ->
+            if (index == 0) {
+                longs.add(0)
+            } else {
+                val valueToAdd = reversedComputations[index - 1].last()
+                longs.add(longs.last() + valueToAdd)
+            }
+        }
+
+        return computations.first().last()
+    }
+}
+
+fun main() {
+    val scans = "2023/day_09".getLines().map { input ->
+        input.split(" ").map { it.toLong() }.let {
+            Scan(
+                report = it
+            )
+        }
+    }
+
+    measureTimeMillis {
+        scans.sumOf {
+            it.extrapolateNumber()
+        }.let {
+            println("Part 1: $it")
+        }
+    }.let {
+        println("Time: $it ms")
+    }
+
+    measureTimeMillis {
+        scans.sumOf {
+            it.extrapolateNumber(reversed = true)
+        }.let {
+            println("Part 2: $it")
+        }
+    }.let {
+        println("Time: $it ms")
+    }
+}
